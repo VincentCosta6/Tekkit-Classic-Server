43baf91efc8016abba98a69709d3f026cef2d73f
Vincent Costa <costa.vincent132@gmail.com>
2018-07-01 16:58:38 -0700

    Player class added with saving and reading to files

--- src/PlayerData/PlayerD.java
+++ src/PlayerData/PlayerD.java
@@ -1,13 +1,17 @@
 package PlayerData;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 import org.bukkit.entity.Player;
 
 public class PlayerD implements Serializable{
-	
-	public ArrayList<PlayerD> players = new ArrayList<PlayerD>();
+	public static String URL = "../Data/Players/";
+	public static ArrayList<PlayerD> players = new ArrayList<PlayerD>();
 
 	private String playerName;
 	private long accountBalance;
@@ -49,5 +53,44 @@ public class PlayerD implements Serializable{
 	{
 		this.permission = newPermission;
 		return this;
+	}
+	
+	public boolean save()
+	{
+		return savePlayer(this);
+	}
+	
+	
+	
+	
+	public static boolean savePlayer(PlayerD p)
+	{
+		try {
+			FileOutputStream fos = new FileOutputStream(URL + p.playerName + ".txt");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(p);
+			oos.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	public static PlayerD readPlayer(String name)
+	{
+		try { 
+			FileInputStream fin = new FileInputStream(URL + name + ".txt");
+			ObjectInputStream ois = new ObjectInputStream(fin);
+			PlayerD player = (PlayerD) ois.readObject();
+			ois.close();
+			return player;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
